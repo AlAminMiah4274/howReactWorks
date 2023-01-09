69675557690a3819e78433e536e56eda859a39d7
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-01-09 22:21:58 +0600

    added new component named display

--- src/components/Watch/Watch.js
+++ src/components/Watch/Watch.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Display from '../Display/Display';
 
 const Watch = () => {
     const [steps, setSteps] = useState(0);
@@ -9,14 +10,15 @@ const Watch = () => {
     }
 
     useEffect(() => {
-        console.log(steps);
+        // console.log(steps);
     }, [steps]);
 
     return (
-        <div>
+        <div style={{ border: '2px solid purple', margin: '10px' }}>
             <h1>I have smart watch</h1>
             <h3>My current steps: {steps}</h3>
             <button onClick={increaseSteps}>De Dour........</button>
+            <Display name={'Garmin'} steps={steps}></Display>
         </div>
     );
 };
